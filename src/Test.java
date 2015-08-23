ad58d64f5d358b51120154b6c6bb3e60200c8d34
nulldev <contactnulldev@gmail.com>
2015-08-23 15:33:21 -0400

    Added UHD 4K and Quad HD resolution. Added human friendly names for video resolutions. Added asserts to tests.

--- src/Test.java
+++ src/Test.java
@@ -15,40 +15,50 @@ import gq.nulldev.libGUC.videos.GUCVideoType;
 public class Test {
 
     static final String EXAMPLE_CONTENT_KEY = "FStqaBaXK7pteZ4jX5poKc0c-Ed2tqKcv2NyTAP7MwuH";
+    //This invalid content key is not really reliable (what happens when a content with that key actually appears)?
     static final String EXAMPLE_INVALID_CONTENT_KEY = "0000000000000000000000000000000000000";
 
     /*
-    Too lazy to write unit tests :/
+    Too lazy to write real unit tests :/
      */
     public static void main(String[] args) {
+        String result;
+        boolean booleanResult;
         System.out.println("Starting tests...");
         System.out.println();
         System.out.println("Testing basic GUC object creation:");
-        System.out.println(new GUCObject(EXAMPLE_CONTENT_KEY));
+        System.out.println(result = new GUCObject(EXAMPLE_CONTENT_KEY).toString());
+        assert result.equals("https://lh3.googleusercontent.com/FStqaBaXK7pteZ4jX5poKc0c-Ed2tqKcv2NyTAP7MwuH");
         System.out.println();
         System.out.println("Testing basic photo creation:");
-        System.out.println(new GUCPhoto(EXAMPLE_CONTENT_KEY));
+        System.out.println(result = new GUCPhoto(EXAMPLE_CONTENT_KEY).toString());
+        assert result.equals("https://lh3.googleusercontent.com/FStqaBaXK7pteZ4jX5poKc0c-Ed2tqKcv2NyTAP7MwuH=s0");
         System.out.println("Testing photo edge pixel limiting:");
         {
             GUCPhotoSize photoSize = new GUCPhotoSize(500);
             GUCPhoto photo = new GUCPhoto(EXAMPLE_CONTENT_KEY, photoSize);
             System.out.println(photo);
+            assert photo.toString().equals("https://lh3.googleusercontent.com/FStqaBaXK7pteZ4jX5poKc0c-Ed2tqKcv2NyTAP7MwuH=s500");
         }
         System.out.println("Testing photo height and width scaling:");
         {
             GUCPhotoSize photoSize = new GUCPhotoSize(100, 300);
             GUCPhoto photo = new GUCPhoto(EXAMPLE_CONTENT_KEY, photoSize);
             System.out.println(photo);
+            assert photo.toString().equals("https://lh3.googleusercontent.com/FStqaBaXK7pteZ4jX5poKc0c-Ed2tqKcv2NyTAP7MwuH=w300-h100");
         }
         System.out.println();
         System.out.println("Testing video types:");
         {
             System.out.println("TINY-3GP:");
-            System.out.println(new GUCVideo(EXAMPLE_CONTENT_KEY, GUCVideoType._3GP_320x180));
+            System.out.println(result = new GUCVideo(EXAMPLE_CONTENT_KEY, GUCVideoType._3GP_320x180).toString());
+            assert result.equals("https://lh3.googleusercontent.com/FStqaBaXK7pteZ4jX5poKc0c-Ed2tqKcv2NyTAP7MwuH=m36");
             System.out.println("SMALL-MP4:");
-            System.out.println(new GUCVideo(EXAMPLE_CONTENT_KEY, GUCVideoType._MP4_640x360));
+            System.out.println(result = new GUCVideo(EXAMPLE_CONTENT_KEY, GUCVideoType._MP4_640x360).toString());
+            assert result.equals("https://lh3.googleusercontent.com/FStqaBaXK7pteZ4jX5poKc0c-Ed2tqKcv2NyTAP7MwuH=m18");
             System.out.println("BIG-MP4:");
-            System.out.println(new GUCVideo(EXAMPLE_CONTENT_KEY, GUCVideoType._MP4_1280x720));
+            System.out.println(result = new GUCVideo(EXAMPLE_CONTENT_KEY, GUCVideoType._MP4_1280x720).toString());
+            assert result.equals("https://lh3.googleusercontent.com/FStqaBaXK7pteZ4jX5poKc0c-Ed2tqKcv2NyTAP7MwuH=m22");
             //My example content is not big enough for the massive MP4 :(
         }
         System.out.println();
@@ -60,29 +70,36 @@ public class Test {
             thumb.animate(true);
             thumb.showPlayButton(false);
             System.out.println(thumb);
+            assert thumb.toString().equals("https://lh3.googleusercontent.com/FStqaBaXK7pteZ4jX5poKc0c-Ed2tqKcv2NyTAP7MwuH=s0-no");
             System.out.println("Non-animated, no play button:");
             thumb = video.getThumbnail();
             thumb.animate(false);
             thumb.showPlayButton(false);
             System.out.println(thumb);
+            assert thumb.toString().equals("https://lh3.googleusercontent.com/FStqaBaXK7pteZ4jX5poKc0c-Ed2tqKcv2NyTAP7MwuH=s0-k-no");
             System.out.println("Animated, play button:");
             thumb = video.getThumbnail();
             thumb.animate(true);
             thumb.showPlayButton(true);
             System.out.println(thumb);
+            assert thumb.toString().equals("https://lh3.googleusercontent.com/FStqaBaXK7pteZ4jX5poKc0c-Ed2tqKcv2NyTAP7MwuH=s0");
             System.out.println("Non-animated, play button:");
             thumb = video.getThumbnail();
             thumb.animate(false);
             thumb.showPlayButton(true);
             System.out.println(thumb);
+            assert thumb.toString().equals("https://lh3.googleusercontent.com/FStqaBaXK7pteZ4jX5poKc0c-Ed2tqKcv2NyTAP7MwuH=s0-k");
         }
         System.out.println();
         System.out.println("Testing content key verifier:");
-        System.out.println(GUCGoogleCommunicator.isContentKeyValid(EXAMPLE_CONTENT_KEY));
-        System.out.println(GUCGoogleCommunicator.isContentKeyValid(EXAMPLE_INVALID_CONTENT_KEY));
+        System.out.println(booleanResult = GUCGoogleCommunicator.isContentKeyValid(EXAMPLE_CONTENT_KEY));
+        assert booleanResult;
+        System.out.println(booleanResult = GUCGoogleCommunicator.isContentKeyValid(EXAMPLE_INVALID_CONTENT_KEY));
+        assert !booleanResult;
         System.out.println();
         System.out.println("Testing getSupportedResolutions:");
-        System.out.println(GUCGoogleCommunicator.getSupportedResolutions(EXAMPLE_CONTENT_KEY));
+        System.out.println(result = GUCGoogleCommunicator.getSupportedResolutions(EXAMPLE_CONTENT_KEY).toString());
+        assert result.equals("[_MP4_1280x738, _MP4_1280x720, _MP4_640x360, _3GP_320x180]");
         System.out.println();
         System.out.println("All tests complete!");
     }
--- src/gq/nulldev/libGUC/videos/GUCVideoType.java
+++ src/gq/nulldev/libGUC/videos/GUCVideoType.java
@@ -9,30 +9,53 @@ package gq.nulldev.libGUC.videos;
 /**
  * Different variants of the size of the video Google can serve.
  *
- * Please note that the video size does not affect the thumbnail
+ * Please note that the video size does not affect the thumbnail.
+ *
+ * Also please note that the videos do not have to be in the MP4 format, they can also be found regularly in WEBM format but
+ * the MP4 format is the most common.
  *
  * WARNING: Do not just use the largest size possible. It will just give you an invalid URL (which doesn't fall back).
  *          Make sure that the original video is at least equal or larger to the size you want served.
  *          To do this, you can use the "getSupportedResolutions(GUCObject video)" function in the GUCGoogleCommunicator class.
  */
 public enum GUCVideoType {
-    _MP4_1920x1080(37),
-    _MP4_1280x738(15),
-    _MP4_1280x720(22),
-    _MP4_640x360(18),
-    _3GP_320x180(36);
+    /* +--------------------------------------------------+ */
+    /* | PLEASE NOTE ABOUT VIDEOS IN 4K                   | */
+    /* |                                                  | */
+    /* | The videos appear to have a split video          | */
+    /* | and audio track at this point!                   | */
+    /* |                                                  | */
+    /* | Also note that the video track is not DCI        | */
+    /* | 4K it is actually UHD 4K.                        | */
+    /* | */ _MP4_3840x2160(272, "Ultra HD 4K"),/* (VIDEO) | */
+    /* |    _AUDIO_3840x2160(251),                (AUDIO) | */
+    /* +--------------------------------------------------+ */
+    _MP4_2560x1440(271, "Quad HD"),
+    _MP4_1920x1080(37, "Full HD"),
+    _MP4_1280x738(15, "HD"),
+    _MP4_1280x720(22, "HD"),
+    _MP4_640x360(18, "SD"),
+    _3GP_320x180(36, "Quarter VGA");
 
     private int code;
+    private String friendlyName;
 
-    GUCVideoType(int code) {
+    GUCVideoType(int code, String name) {
         this.code = code;
+        this.friendlyName = name;
     }
 
     /**
-     * Get the secret video codes that google uses to identify the different types.
+     * Get the secret video codes that Google uses to identify the different types.
      * @return The video code.
      */
     public int getCode() {
         return code;
     }
+
+    /**
+     * Get a more human friendly name of the video format.
+     * @return A human friendly name of the video format.
+     */
+    public String getFriendlyName() { return friendlyName; }
 }
